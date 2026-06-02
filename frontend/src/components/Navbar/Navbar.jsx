0abf5deca721fe9abf6d8efe2260de5da667275a
SanthoshaNaik <santhoshanaik02@gmail.com>
2026-06-02 20:08:32 +0530

    implement mobile responsive view layouts and navigation menu

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css";
 //import "./App.css";
 
@@ -6,26 +6,33 @@ import logo from '../Assets/GLogo.png'
 import cart_icon from '../Assets/cartLogo.png'
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <div className='navBarWrapper'>
-            {/* <nav className='navBar'> */}
-                <div className='nav-logo'>
-                   <a href='/home'> <img src={logo} alt='' width={100} height={95} /> </a>
-                </div>
-                
-                <a href='/home' className='links'>Home</a>
-                
-                <a href='/about-us' className='links'>About Us</a>
-                <a href="/contactus" className='links'>Contact Us</a>
-                
-                <a href='/products' className='links'>Our Products</a>
-                
-                <a href='/LoginSignup' className='links'>Login</a>
-                <div className='nav-cart'>
-                 <a href='/Cart' >  <img src={cart_icon} alt='' width={90} height={90} /> </a>
-                    <div className="nav-cart-count">0</div>
-                </div>
-            {/* </nav> */}
+            <div className='nav-logo'>
+               <a href='/home'> <img src={logo} alt='' width={100} height={95} /> </a>
+            </div>
+
+            {/* Hamburger Menu Toggle Button */}
+            <div className={`nav-toggle ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+            
+            <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+                <a href='/home' className='links' onClick={() => setMenuOpen(false)}>Home</a>
+                <a href='/about-us' className='links' onClick={() => setMenuOpen(false)}>About Us</a>
+                <a href="/contactus" className='links' onClick={() => setMenuOpen(false)}>Contact Us</a>
+                <a href='/products' className='links' onClick={() => setMenuOpen(false)}>Our Products</a>
+                <a href='/LoginSignup' className='links' onClick={() => setMenuOpen(false)}>Login</a>
+            </div>
+
+            <div className='nav-cart'>
+             <a href='/Cart' >  <img src={cart_icon} alt='' width={90} height={90} /> </a>
+                <div className="nav-cart-count">0</div>
+            </div>
         </div>
     )
 }
