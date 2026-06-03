dac5c558e60963ae430e9f49f6d43ba32d964405
SanthoshaNaik <santhoshanaik02@gmail.com>
2026-06-03 11:47:46 +0530

    add premium hero section with grocery basket image and floating animation on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,21 @@
 import React from 'react'
 import './pagesCSS/Home.css'
+import groceryHero from '../components/Assets/grocery_hero.png'
 
 function Home() {
     return (
         <div className="home-page">
 
             <div className="section1">
-  
-                    <p className='homeText'>FUEL YOUR BODY NOURISH YOUR SOUL..!</p>
-                    
-                    <a href='/products'> <button className='hbtn'>Explore Here!..</button> </a>
-
+                <span className="welcome-tag">WELCOME TO FRESH MART</span>
+                <p className='homeText'>FUEL YOUR BODY<br />NOURISH YOUR SOUL..!</p>
+                <p className="homeSubText">Discover the freshest organic fruits, vegetables, and pantry essentials delivered straight to your doorstep.</p>
+                <a href='/products'> <button className='hbtn'>Explore Here!..</button> </a>
             </div>
             
             <div className="section2">
-                <h1>FRESH-MART</h1>
-                {/* <p>ಏನ್ರೀ ಮೀಡಿಯಾ..!</p> */}
-            
-                
+                <img src={groceryHero} alt="Fresh Mart Groceries" className="home-hero-img" />
             </div>
-
-         
-         
-
-           
-           
-    
 
         </div>
         
